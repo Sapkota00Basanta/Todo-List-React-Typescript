f1b428296ee074ab1f02a549c9f31ff2142609c1
Basanta Sapkota <sapkota.basanta46@gmail.com>
2023-03-08 21:56:27 +0545

    Added react-router version 6

---
+++ src/types/App.types.ts
@@ -0,0 +1,6 @@
+/**
+ * This module consists of all the type decleration for App Component
+ */
+export interface IAppProps {
+  props?: any;
+}
---
+++ src/types/components/Listview.types.ts
@@ -0,0 +1,6 @@
+/**
+ * This module consists of all the type decleration for Listview Component
+ */
+export interface IListViewProps {
+  props?: any;
+}
