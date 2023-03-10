21280114a87f9f73008110828532c8061b4d4e28
Basanta Sapkota <sapkota.basanta46@gmail.com>
2023-03-10 13:52:28 +0545

    Added simple state sharing method from higher component

--- src/components/Focus/Focus.tsx
+++ src/components/Focus/Focus.tsx
@@ -1,17 +1,13 @@
 // Import Third-Party Modules
-import React from "react";
+import React from 'react';
 
 // Import User-Defined Modules
-import { IFocusProps } from "../../types/components/Focus.types";
+import { IFocusProps } from '../../types/components/Focus.types';
 
 /**
  * This module consist of focus view displaying single focused todo list
  * @returns Focus View Component
  */
-export const Focus: React.FC<IFocusProps> = () => {
-  return (
-    <>
-      <div> Focus View</div>
-    </>
-  );
+export const Focus: React.FC<IFocusProps> = ({ tasks }) => {
+  return tasks[0] ? <>{tasks[0].label}</> : <>No tasks found.</>;
 };
